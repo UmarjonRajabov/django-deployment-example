11ae5df23800f7cf2aa1cff61c3ee81cf9f2d138
Umarjon Rajabov <U_Rajabov@nbu.local>
2024-02-21 17:47:31 +0500

    Change sizes ...

--- calc/signals.py
+++ calc/signals.py
@@ -3,10 +3,12 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Employee
 
+
 # Create or update Employee instances upon user creation/update
 @receiver(post_save, sender=get_user_model())
 def create_or_update_employee(sender, instance, created, **kwargs):
     if created:
         Employee.objects.create(user=instance)
     else:
-        instance.employee.save()
+        if hasattr(instance,'employee'):
+            instance.employee.save()
