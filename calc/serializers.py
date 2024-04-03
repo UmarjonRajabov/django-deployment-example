7ca2df6ea044af1b800f7ed85acb7bcb7404ec89
Umarjon Rajabov <U_Rajabov@nbu.local>
2024-04-03 12:44:20 +0500

    Some changes...

--- calc/serializers.py
+++ calc/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import KPI
+from .models import KPI, KPIArchive
 
 
 class KPISerializer(serializers.ModelSerializer):
     class Meta:
         model = KPI
+        fields = '__all__'
+
+
+class KPIArchiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KPIArchive
         fields = '__all__'
