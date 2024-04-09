bf7c4af4edc2ec696d26cec31be82bcdafe10718
Umarjon Rajabov <U_Rajabov@nbu.local>
2024-04-09 18:20:24 +0500

    Some changes...

--- back_project/settings.py
+++ back_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'calc',
     'rest_framework',
-    'drf-yasg',
+    # 'drf-yasg',
 
 ]
 
--- back_project/urls.py
+++ back_project/urls.py
@@ -18,22 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="KPI list Api",
-        default_version='kpis',
-        description='KPI demo project',
-        terms_of_service='demo.com',
-        contact=openapi.Contact(email='umarjon.rajabov@gmail.com'),
-        license=openapi.License(name='demo licence'),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny, ],
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="KPI list Api",
+#         default_version='kpis',
+#         description='KPI demo project',
+#         terms_of_service='demo.com',
+#         contact=openapi.Contact(email='umarjon.rajabov@gmail.com'),
+#         license=openapi.License(name='demo licence'),
+#     ),
+#     public=True,
+#     permission_classes=[permissions.AllowAny, ],
+# )
 
 urlpatterns = [
     path('api/', include('calc.urls')),
@@ -42,10 +42,10 @@ urlpatterns = [
     path('', include('calc.urls')),
 
     # swagger
-    path('swagger/',schema_view.with_ui(
-        'swagger', cache_timeout=0), name='swagger-swagger-ui'),
-    path('redoc/', schema_view.with_ui(
-        'redoc', cache_timeout=0), name='schema-redoc')
+    # path('swagger/',schema_view.with_ui(
+    #     'swagger', cache_timeout=0), name='swagger-swagger-ui'),
+    # path('redoc/', schema_view.with_ui(
+    #     'redoc', cache_timeout=0), name='schema-redoc')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
