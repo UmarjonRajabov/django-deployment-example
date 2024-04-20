63eeda5956afeb5223f88dbca7e63ea6e27e592d
Umarjon <47208541+UmarjonRajabov@users.noreply.github.com>
2024-04-20 12:56:08 +0500

    local changes

--- calc/views.py
+++ calc/views.py
@@ -327,5 +327,6 @@ def kpi_card(request):
     return render(request, 'kpi/card.html')
 
 # 4. Allow Users to View Previous Months
+
 # You can implement a dropdown menu or any other UI element in your template
 # to allow users to select and view data for previous months.
